e9d6a765f64c1c289be195cabd9988e18cc8a22d
fernandesleticia <leticiafernandes777@gmail.com>
2020-01-07 23:09:02 -0300

    moving the ship

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('assets/space-invaders-player-64px.png')
 playerX = 400
 playerY = 500
+playerX_change = 0
 
-def player():
-	screen.blit(playerImg, (playerX,playerY))
+def player(x,y):
+	screen.blit(playerImg, (x,y))
 
 # game loop
 running = True
@@ -27,11 +28,22 @@ while running:
 
 	# RGB 
 	screen.fill((0,0,0))
-
+    
+	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				playerX_change = -0.1	
+			if event.key == pygame.K_RIGHT:
+				playerX_change = 0.1			
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				print("keystoke")		
 	
-	player()
+	playerX += playerX_change
+	player(playerX, playerY)
 	pygame.display.update()
 
